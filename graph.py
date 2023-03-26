2f348720b5bab91756109a82cc0698212d10f6ac
Kate Holdener <kate.holdener@slu.edu>
2023-03-26 11:45:44 -0500

    added conversion of inputs to integers

--- graph.py
+++ graph.py
@@ -30,8 +30,8 @@ class Graph:
 if __name__ == '__main__':
 
      params = input().rstrip().split()
-     start_node =  params[0];
-     distance = params[1]
+     start_node =  int(params[0])
+     distance = int(params[1])
      first_multiple_input = input().rstrip().split()
      n = int(first_multiple_input[0])
      m = int(first_multiple_input[1])
@@ -41,6 +41,6 @@ if __name__ == '__main__':
 
      for e in range(m):
         edge = input().rstrip().split()
-        g.add_edge(edge[0],edge[1])
+        g.add_edge(int(edge[0]),int(edge[1]))
 
      reachable = g.get_reachable_nodes(start_node, distance)
